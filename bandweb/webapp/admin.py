b7b589f9ac2d635570ab79ad040dca5ad9fdbf01
Álvaro Medina Ballester <alvaro@comiendolimones.com>
2011-10-09 23:40:41 +0200

    Admin añadido

---
+++ bandweb/webapp/admin.py
@@ -0,0 +1,15 @@
+from webapp.models import *
+from django.contrib import admin
+
+admin.site.register(Categorie)
+admin.site.register(News)
+admin.site.register(Album)
+admin.site.register(Track)
+admin.site.register(Tour)
+admin.site.register(Show)
+admin.site.register(Journal)
+admin.site.register(Biography)
+admin.site.register(PhotoAlbum)
+admin.site.register(Photo)
+admin.site.register(Discussion)
+admin.site.register(Comment)
--- bandweb/webapp/models.py
+++ bandweb/webapp/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 # Models for the user and content sections
 class Categorie(models.Model):
     name = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.name
 
 class News(models.Model):
     title = models.CharField(max_length=350)
@@ -11,24 +13,32 @@ class News(models.Model):
     content = models.TextField()
     categorie = models.ForeignKey(Categorie)
     author = models.ForeignKey(User)
+    def __unicode__(self):
+        return self.title + self.pub_date
 
 class Album(models.Model):
     title = models.CharField(max_length=1000)
     rel_date = models.DateTimeField()
     tracks = models.PositiveSmallIntegerField()
     art = models.ImageField(upload_to='art/%Y/%m')
+    def __unicode__(self):
+        return self.title
 
 class Track(models.Model):
     title = models.CharField(max_length=1000)
     lyrics = models.TextField(blank=True)
     duration = models.TimeField(null=True)
     album = models.ForeignKey(Album)
+    def __unicode__(self):
+        return self.title
 
 class Tour(models.Model):
     name = models.CharField(max_length=500)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     description = models.TextField(blank=True)
+    def __unicode__(self):
+        return self.name + self.start_date
     
 class Show(models.Model):
     date = models.DateTimeField()
@@ -37,24 +47,32 @@ class Show(models.Model):
     info = models.TextField(blank=True)
     headline = models.NullBooleanField()
     tour = models.ForeignKey(Tour, null=True)
+    def __unicode__(self):
+        return self.date + self.location
 
 class Journal(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User)
     text = models.TextField()
     tour = models.ForeignKey(Tour)
+    def __unicode__(self):
+        return self.pub_date + self.author
 
 class Biography(models.Model):
     text = models.TextField()
     hometown = models.CharField(max_length=300)
     genre = models.CharField(max_length=300)
     label = models.CharField(max_length=300)
+    def __unicode__(self):
+        return self.text
 
 class PhotoAlbum(models.Model):
     name = models.CharField(max_length=300)
     summary = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    def __unicode__(self):
+        return self.name
 
 class Photo(models.Model):
     title = models.CharField(max_length=256)
@@ -64,12 +82,16 @@ class Photo(models.Model):
     image = models.ImageField(upload_to='photos/%Y/%m')
     photo_album = models.ForeignKey(PhotoAlbum, null=True)
     is_cover_photo = models.BooleanField()
+    def __unicode__(self):
+        return self.title
 
 class Discussion(models.Model):
     reply_to = models.ForeignKey("self", null=True)
     text = models.TextField()
     autor = models.ForeignKey(User)
-    
+    def __unicod__(self):
+        return self.text + self.author
+ 
 class Comment(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -77,4 +99,5 @@ class Comment(models.Model):
     news = models.ForeignKey(News, null=True)
     album = models.ForeignKey(Album, null=True)
     show = models.ForeignKey(Show, null=True)
-
+    def __unicode__(self):
+        return self.text
